621617febad8dddb6055d41345fd91f2a51b9cd0
Diego Lirio <diegolirio.dl@gmail.com>
2020-07-08 11:22:46 -0300

    I Created ConfigServer and ConfigStore

---
+++ aws-config-server/src/main/java/br/com/lirio/awsconfigserver/AwsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lirio.awsconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsConfigServerApplication.class, args);
+	}
+
+}
