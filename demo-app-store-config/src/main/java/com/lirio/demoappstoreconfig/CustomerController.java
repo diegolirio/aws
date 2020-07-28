e210fd73f4fd21c965b6820806cd41190b039890
Diego Lirio <diegolirio.dl@gmail.com>
2020-07-28 12:13:31 -0300

    Spring Cloud Parameters Store

---
+++ demo-app-store-config/src/main/java/com/lirio/demoappstoreconfig/CustomerController.java
@@ -0,0 +1,25 @@
+package com.lirio.demoappstoreconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class CustomerController {
+
+    @Value("${customer.name}")
+    private String name;
+
+    @Value("${customer.age}")
+    private String age;
+
+    @GetMapping
+    public List<String> get() {
+        return List.of(name, age);
+    }
+
+}
---
+++ demo-app-store-config/src/main/java/com/lirio/demoappstoreconfig/DemoAppStoreConfigApplication.java
@@ -0,0 +1,13 @@
+package com.lirio.demoappstoreconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppStoreConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppStoreConfigApplication.class, args);
+	}
+
+}
