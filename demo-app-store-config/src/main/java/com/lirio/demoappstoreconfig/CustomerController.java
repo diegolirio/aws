1be09571cdaad2dcaeb44fe830600479e3696ddf
Diego Lirio <diegolirio.dl@gmail.com>
2020-07-30 22:38:51 -0300

    Spring Cloud AWS Secrets Manager and Refresh

--- demo-app-store-config/src/main/java/com/lirio/demoappstoreconfig/CustomerController.java
+++ demo-app-store-config/src/main/java/com/lirio/demoappstoreconfig/CustomerController.java
@@ -1,12 +1,14 @@
 package com.lirio.demoappstoreconfig;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping
 public class CustomerController {
